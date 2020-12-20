f788255cc8c536ed96e4fd7333fae78b29e6dbdd
Benjamin <omben001@cougars.csusm.edu>
2020-12-20 01:27:47 -0800

    Adds Page Navigation

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,16 @@
 import React, {useState} from 'react'
 import ThemeToggler from './ThemeToggler'
 import navbar from '../data/NavBar'
-import NavItem from "./NavItem"
+import NavItem from './NavItem'
 import { ThemeInfo } from '../interfaces/NavBar.i'
 
-const expanded = "navigation-bar-expanded"
-const shrunk = "navigation-bar-shrunk"
+const expanded = "navigation-bar-expanded" // Represents the CSS class containing the animation that expands the navigation bar.
+const shrunk = "navigation-bar-shrunk"     // Represents the CSS class containing the animation that shrinks the navigation bar.
 
 function Navbar({theme, setTheme}: ThemeInfo){
     const [navState, setNavState] = useState(expanded)
 
+    // Changes the state of the navigation bar from expanded to shrunk. 
     const shrink = () => {
         if(navState === shrunk){
             return;
@@ -18,6 +19,7 @@ function Navbar({theme, setTheme}: ThemeInfo){
         }
     }
 
+    // Changes the state of the navigation bar from shrunk to expanded.
     const expand = () => {
         if(navState === expanded){
             return
@@ -26,27 +28,33 @@ function Navbar({theme, setTheme}: ThemeInfo){
         }
     }
 
+    // Renders Benjamin's portrait as an image.
+    const portrait = (link: string) : JSX.Element => {
+        return  <img className="portrait" src={link} alt="Benjamin's Portrait" />
+    }
+
+    // Renders Benjamin's initials
+    const initials = (name: string): JSX.Element =>{
+        return <h2>{name}</h2>
+    }
+
     return(
         <div className={`${navState} flex-space-around`}>
             <div>
-                <h2>{navbar.initials.name}</h2>
+                {navState === expanded && portrait(navbar.initials.link)}
+                {navState === shrunk && initials(navbar.initials.name) }
             </div>
             <div className="flex-space-between">
                 <div className="flex-space-around">
-                    <NavItem name={navbar.home.name} link={navbar.home.link} />
-                    <NavItem name={navbar.work.name} link={navbar.work.link} />
-                    <NavItem name={navbar.interests.name} link={navbar.interests.link} />
-                    <NavItem name={navbar.resume.name} link={navbar.resume.link} />
-                    <div className="pointer" onClick={expand}>|| Home |</div>
-                    <div className="pointer" onClick={shrink}>| Work |</div> 
-                    <div className="pointer" onClick={shrink}>| Interests ||</div>   
+                    <NavItem name={navbar.home.name} link={navbar.home.link} animation={expand} />
+                    <NavItem name={navbar.work.name} link={navbar.work.link} animation={shrink} />
+                    <NavItem name={navbar.interests.name} link={navbar.interests.link} animation={shrink} />
+                    <NavItem name={navbar.resume.name} link={navbar.resume.link} animation={expand} /> 
                 </div>
                 <div className="theme-toggler">
                     <ThemeToggler theme={theme} setTheme={setTheme} />
                 </div>
-            </div>
-            
-           
+            </div> 
         </div>
     )
 }
