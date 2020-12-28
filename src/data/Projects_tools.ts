53050dc4bf903123b78c09b6f079368518aa6d96
Benjamin <omben001@cougars.csusm.edu>
2020-12-28 12:11:16 -0800

    Adds project tools file

---
+++ src/data/Projects_tools.ts
@@ -0,0 +1,46 @@
+/**
+ * This file has the list of tools used to build each project
+ */
+
+import colors from '../data/tools_colors.json'
+
+export const cards_cast_tools = [
+    {
+        name: "HTML",
+        color: colors.HTML
+    },
+    {
+        name: "CSS",
+        color: colors.CSS
+    },
+    {
+        name: "JavaScript",
+        color: colors.JavaScript
+    }
+]
+
+export const lm_video_tour_tools = [
+    {
+        name: "JavaScript",
+        color: colors.JavaScript
+    }
+]
+
+export const trace_c19_tools = [
+    {
+        name: "HTML",
+        color: colors.HTML
+    },
+    {
+        name: "CSS",
+        color: colors.CSS,
+    },
+    {
+        name: "JavaScript",
+        color: colors.JavaScript
+    },
+    {
+        name: "Java",
+        color: colors.Java
+    }
+]
